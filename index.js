0766ec4758fd4c138ce184ebf2d0727a5638b36b
rego467 <regoedwar.1@gmail.com>
2023-12-05 11:26:56 +0700

    menambah paginations

--- index.js
+++ index.js
@@ -4,70 +4,71 @@ const PUBLIC_KEY = "91a29dee29914d48943b04b24abb73a0"
 let logout = document.querySelector(".logout")
 let next = document.querySelector(".next")
 
-let prev = document.querySelector(".prev")
+let prev = document.getElementById("prev")
 let cardResult = document.querySelector(".card-result")
 
 let loading = document.querySelector(".loading")
 let buttonActions = document.querySelector(".button-actions")
 
 let resultCount = document.querySelector('.result-count')
-
+let numberPage = document.querySelector(".numberPage")
+let numberOfPage = document.querySelector(".numberOfPage")
 
 // const dummy = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQTJvv_C0eeDiv043l5pWil4KsV0t_YQ1k2g&usqp=CAU"
 // const dummyHeading = "lorem ipsum"
 // const woiIni = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
 
 class Count{
-  static count = 10
+  static count = 1
 
   static increment(){
-    return this.count = this.count + 5
+    return this.count = this.count + 1
   }
 
   static decrement(){
-    if(this.count === 10){
+    if(this.count === 1){
       this.count
     }else{
-      this.count = this.count - 5
+      this.count = this.count - 1
+      console.log("woii")
+    }
+  }
+}
+
+class LastCount{
+  static countL = 1041
+  
+  static increment(){
+    prev.disabled = false
+    this.countL = this.countL - 1
+  }
+
+  static decrement(){
+    if(this.countL === 1041){
+      this.countL
+      prev.disabled = true      
+    }else{
+      this.countL = this.countL + 1
     }
   }
 }
 
 
 async function getApi(){
-  const url =`https://api.jikan.moe/v4/top/anime`
+  const url =`https://api.jikan.moe/v4/top/anime?page=${Count.count}`
   try {
     const response = await fetch(url, {method: "GET"})
     const resJson =  await response.json()
-console.log(resJson)
-    return resJson
+    console.log(resJson)
+    displayArticle(resJson.data)
   } catch (error) {
     console.log(error.message)
   }
 }
 
-// function randomData(dataAray){
-//   let result = [];
-//     for (let index = 0; index < dataAray.length; index++) {
-//       const random = Math.floor(Math.random() * dataAray.length)
-//       result.push(dataAray[random])
-//     }
-//   return result  
-// }
-
-
-async function displayArticle(){
-  const getData = await getApi()
-  // // const randomNews = randomData(data.articles)
-  // if (!data || !data.articles || !Array.isArray(data.articles)) {
-  //   console.error("Invalid data structure received from API.");
-  //   return;
-  // }
-
+function displayArticle(data){
   cardResult.innerHTML= ""
-
-  getData.data
-    .slice(0, Count.count)
+  data
     .map((item)=>{
       cardResult.innerHTML +=`
       <div class="card">
@@ -88,24 +89,32 @@ function renderPertamaKali(){
     loading.textContent ="loading..."
     buttonActions.classList.add("hidden")  
     setTimeout(()=>{
-      displayArticle()
+      getApi()
+      numberPage.textContent = Count.count
+      numberOfPage.textContent = LastCount.countL
       loading.textContent = ""
       buttonActions.classList.remove("hidden")
     },2000)
   }
 
 }
-
 renderPertamaKali()
 
 
 function nextButton(){
   Count.increment()
-  displayArticle()
+  LastCount.increment()
+  getApi()
+  numberPage.textContent = Count.count
+  numberOfPage.textContent = LastCount.countL
 }
+
 function prevButton(){
+  LastCount.decrement()
   Count.decrement()
-  displayArticle()
+  getApi()
+  numberPage.textContent = Count.count
+  numberOfPage.textContent = LastCount.countL
 }
 
 
@@ -118,3 +127,13 @@ function logoutButton(){
 prev.addEventListener("click", prevButton)
 next.addEventListener("click", nextButton)
 logout.addEventListener("click",logoutButton)
+
+
+// function randomData(dataAray){
+//   let result = [];
+//     for (let index = 0; index < dataAray.length; index++) {
+//       const random = Math.floor(Math.random() * dataAray.length)
+//       result.push(dataAray[random])
+//     }
+//   return result  
+// }
